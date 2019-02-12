b09425a9cd62103b4a8e1beb608d55bddc230a17
baur <baur@baurs-MacBook-Pro.local>
2019-02-12 11:36:51 -0600

    initial commit. Students and Schedule routes added for JSON data consumption

---
+++ courseSortService.js
@@ -0,0 +1,93 @@
+Last login: Wed Nov 21 15:48:51 on ttys034
+baurs-MacBook-Pro:admissionsApp baur$ npm install
+npm WARN saveError ENOENT: no such file or directory, open '/Users/baur/Documents/admissionsApp/package.json'
+npm notice created a lockfile as package-lock.json. You should commit this file.
+npm WARN enoent ENOENT: no such file or directory, open '/Users/baur/Documents/admissionsApp/package.json'
+npm WARN admissionsApp No description
+npm WARN admissionsApp No repository field.
+npm WARN admissionsApp No README data
+npm WARN admissionsApp No license field.
+
+up to date in 0.114s
+baurs-MacBook-Pro:admissionsApp baur$ npm run
+baurs-MacBook-Pro:admissionsApp baur$ npm install
+npm WARN saveError ENOENT: no such file or directory, open '/Users/baur/Documents/admissionsApp/package.json'
+npm WARN enoent ENOENT: no such file or directory, open '/Users/baur/Documents/admissionsApp/package.json'
+npm WARN admissionsApp No description
+npm WARN admissionsApp No repository field.
+npm WARN admissionsApp No README data
+npm WARN admissionsApp No license field.
+
+up to date in 0.077s
+baurs-MacBook-Pro:admissionsApp baur$ nod index.js
+-bash: nod: command not found
+baurs-MacBook-Pro:admissionsApp baur$ node index.js
+test
+baurs-MacBook-Pro:admissionsApp baur$ claer
+-bash: claer: command not found
+baurs-MacBook-Pro:admissionsApp baur$ cbbbbbbbbbbaurs-MacBook-Pro:admissionsApp baur$ clear
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+baurs-MacBook-Pro:admissionsApp baur$ node index.js
+/Users/baur/Documents/admissionsApp/index.js:33
+console.log( coursesLEftToTake(courseList, coursesTaken) );
+        ^
+
+ReferenceError: coursesLEftToTake is not defined
+    at Object.<anonymous> (/Users/baur/Documents/admissionsApp/index.js:33:9)
+    at Module._compile (module.js:652:30)
+    at Object.Module._extensions..js (module.js:663:10)
+    at Module.load (module.js:565:32)
+    at tryModuleLoad (module.js:505:12)
+    at Function.Module._load (module.js:497:3)
+    at Function.Module.runMain (module.js:693:10)
+    at startup (bootstrap_node.js:188:16)
+    at bootstrap_node.js:609:3
+baurs-MacBook-Pro:admissionsApp baur$ node index.js
+[ 'ID-280', 'ID-290', 'ID-330', 'WD-290' ]
+baurs-MacBook-Pro:admissionsApp baur$ node index.js
+[ 'WD-285', 'MA-260', 'MA-320', 'WD-420' ]
+baurs-MacBook-Pro:admissionsApp baur$ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const students = require('./data/students.json');
+const schedule = require('./data/degreeSchedule.json');
+
+
+const app = express();
+
+let port = process.env.PORT || 3001;
+app.set('json spaces', 2);
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/students', function(req, res){
+    res.send(students);
+});
+app.get('/schedule', function(req, res){
+    res.send(schedule);
+});
+
+// app.get('/about', function(req, res){
+//     res.sendFile(__dirname + '/pages/about.html');
+// });
+
+// app.get('/students/:social', function(req, res){
+//     res.send('Your Student has a Social Security Number of: ' + req.params.social + '</i>');
+// });
+
+// app.get('/anagrams/:word',makeAnagrams, makelowerCase, checkRepeats);
+
+// function makeAnagrams(req,res,next){
+//     function anagrams(word, qt){
+//         var newAnagramArray = [];
+//         for(var i = 0; i < qt; i++){
+//             newAnagramArray.push(word);
+//         }
+    
+//         newAnagramArray = newAnagramArray.map(function(w){
+//             var originalWord = w.split('');
+//             var oldWord = w.split('');
+//             var newWord = [];
+//             newWord = originalWord.map(function(l){
+//                 return oldWord.splice(Math.floor(Math.random() * oldWord.length),1)[0];
+//             });
+//             return newWord.join('');
+//         });
+    
+//         return newAnagramArray;
+//     }
+//     res.locals.createdAnagrams = anagrams(req.params.word, 6);
+//     next();
+// }
+// function makelowerCase(req,res,next){
+//     res.locals.lowerCaseWords = res.locals.createdAnagrams.map( word => word.toLowerCase() );
+//     next();
+// }
+// function checkRepeats(req,res,next){
+//     res.send("We will check for repeats in a minute but here are your angrams: " + String( res.locals.lowerCaseWords ));
+//     res.end();
+// }
+
+app.listen(port);
